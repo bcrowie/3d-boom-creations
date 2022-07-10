8e036fb4fb3066b01374ded33629e40d132eb81b
Brenden Crowie <brendencrowie@gmail.com>
2022-07-10 01:15:54 -0400

    Removed Button component: redundancy

--- src/components/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-
-import "../../styles/Components/Button/Button.scss"
-const Button: React.FC = () => {
-    return (
-        <button type="submit" onClick={(e) => console.log(e)}>Submit</button>
-    )
-}
-
-export default Button
