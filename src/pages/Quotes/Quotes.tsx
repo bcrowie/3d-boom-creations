fdcbd87b1776982d24d7a8ac1720783c31a76293
Brenden Crowie <brendencrowie@gmail.com>
2022-07-10 01:27:49 -0400

    Added basic style to Quote header

--- src/pages/Quotes/Quotes.tsx
+++ src/pages/Quotes/Quotes.tsx
@@ -3,7 +3,10 @@ import "../../styles/Pages/Quotes.scss"
 const Quotes: React.FC = () => {
     return (
         <div className="quotes">
-            <p>This page is for client quotes.</p>
+           <div className="quote-summary">
+            <h1>Get a Quote!</h1>
+            <p>Request a quote Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, dolores voluptatum ullam officiis quae ipsa fugiat accusamus iusto impedit velit voluptates quia? Optio asperiores, esse fugiat autem magni quos sunt consectetur distinctio! Quam repellendus tempore inventore excepturi. Tempora, numquam nisi!</p>
+           </div>
         </div>
     );
 };
