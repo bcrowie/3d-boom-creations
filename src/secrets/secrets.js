4e9bb06ce0008e51ad6686671a32317fdc1c23c3
Brenden Crowie <brendencrowie@gmail.com>
2022-07-11 22:43:45 -0400

    Delete secrets.js

--- src/secrets/secrets.js
+++
@@ -1,5 +0,0 @@
-// EmailJS
-export const emailJsContactTemplateID = "template_7mg4jzu"
-export const emailJsPublicKey = "4qCLVxDyqmRknyJ1C"
-export const emailJsQuoteTemplateID = "template_czg11hf"
-export const emailJsServiceID = "service_rxgizmp"
